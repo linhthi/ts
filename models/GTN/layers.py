0f56668405dc4ed14a06a6b3956c9ea83f990b1f
linhthi <hoanglinh831999@gmail.com>
2021-03-29 17:08:26 +0700

    add GCN layer

--- models/GTN/layers.py
+++ models/GTN/layers.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -162,3 +163,39 @@ class GraphTransformerLayer(nn.Module):
                                                                                    self.in_channels,
                                                                                    self.out_channels, self.num_heads,
                                                                                    self.residual)
+
+
+class GraphConvolution(nn.Module):
+    """
+    Graph Convolution layer
+    """
+
+    def __init__(self, in_dim: int, out_dim: int, use_bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.weight = nn.Parameter(torch.FloatTensor(in_dim, out_dim))
+
+        if use_bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_dim))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return '{}(in_dim={}, out_dim={})'.format(self.__class__.__name__, self.in_dim, self.out_dim)
