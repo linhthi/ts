c1f7e0825c19a01496d7e00d8fe3b6d5e0adbf77
linhht <hoanglinh831999@gmail.com>
2021-04-07 12:55:55 +0700

    updated train gtn

--- main_gtn.py
+++ main_gtn.py
@@ -6,11 +6,11 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
 
 from models.GTN.GTN import GTN
+from models.GTN.sampler import Sampler_GCN
 import utils.utils as utils
 from torch.utils.tensorboard import SummaryWriter
 
@@ -42,59 +42,84 @@ def get_args():
     return args
 
 
-def train(epoch, fastmode, features, train_set, n_users, n_nodes):
+def train(epoch, fastmode, features, train_set, val_set, idx_val, n_users, n_nodes, device, model):
     t = time.time()
     model.train()
     optimizer.zero_grad()
     count = 0
     for batch in train_set:
         idx_batch = utils.get_idx(batch.numpy(), n_users, n_nodes)
-        
         batch_features = features[idx_batch]
-        adj_indices = batch[:, 0:2]
-        adj_values = [1 for i in range(batch.shape[0])]
-        adj_batch = torch.sparse_coo_tensor(adj_indices, adj_values, (idx_batch.shape[0], idx_batch.shape[0]))
-        # TODO: get adjency matrix from batch nodes (also have user-relation)
+        layer_sizes = [batch_features.shape[0], batch_features.shape[0]]
+        sampler = Sampler_GCN(None, features, adj, input_dim=1, 
+                          layer_sizes=layer_sizes, device=device)
+        bf, adj_batch, _ = sampler.sampling(idx_batch)
         
-        score = model(batch_features, adj_batch, batch[:, 0:1], batch[:, 1:2])
-        loss_train = criterion(score, batch[:, 3:4])
+        nodes_u, nodes_v = [], []
+        idx_batch = idx_batch.tolist()
+        for data in batch:
+            u = int(data[0].reshape(1))
+            v = int(data[1].reshape(1))
+            for i in range(len(idx_batch)):
+                if idx_batch[i] == u:
+                    nodes_u.append(i)
+                if idx_batch[i] == v + n_users:
+                    nodes_v.append(i)
+        score = model(batch_features, adj_batch[0], nodes_u, nodes_v)
+        loss_train = criterion(score, batch[:, 3:4].type(torch.FloatTensor))
         count += 1
         # TODO: calculate mae metric
         rmse_train = torch.sqrt(loss_train)
         loss_train.backward()
         optimizer.step()
-        print("Epoch %d/%d: loss_train: %0.4f, rmse_train: %0.4f" %(count, epoch, loss_train, rmse_train))
-       
+        if count % 1000 == 0:
+            print("Epoch %d/%d: loss_train: %0.4f, rmse_train: %0.4f" %(count, epoch, loss_train, rmse_train))
+
+    loss_val, rmse_val = 0, 0
     if not fastmode:
         # Evaluate validation set performance separately,
         # deactivates dropout during validation run.
         model.eval()
-        adj_val_indices = val_set[:, 0:2]
-        adj_val_values = [1 for i in range(val_set.shape[0])]
-        adj_val = torch.sparse_coo_tensor(adj_val_indices, adj_val_values, (n_nodes, n_nodes))
-        score = model(val_features, adj_val, val_set[:, 0:1], val_set[:, 1:2])
-
-    loss_val = criterion(score, val_set[:, 3:4])
-    rmse_val = torch.sqrt(loss_val)
+        val_features = features[idx_val]
+        bf, adj_val, _ = sampler.sampling(idx_val)
+        
+        nodes_u, nodes_v = [], []
+        idx_val = idx_val.tolist()
+        for data in val_set:
+            u = int(data[0].reshape(1))
+            v = int(data[1].reshape(1))
+            for i in range(len(idx_val)):
+                if idx_val[i] == u:
+                    nodes_u.append(i)
+                if idx_val[i] == v + n_users:
+                    nodes_v.append(i)
+        score = model(val_features, adj_val[0], nodes_u, nodes_v)
+        loss_val = criterion(score, val_set[:, 3:4].type(torch.FloatTensor))
+        rmse_val = torch.sqrt(loss_val)
     return (loss_train, loss_val, rmse_train, rmse_val)
 
 
-def test():
+def test(features, test_set, idx_test, n_users):
     model.eval()
-    adj_test_indices = val_set[:, 0:2]
-    adj_test_values = [1 for i in range(test.shape[0])]
-    adj_test = torch.sparse_coo_tensor(adj_test_indices, adj_test_values, (len(test_set), len(test_set)))
-    score = model(tests_features, adj_test, test_set[:, 0:1], test_set[:, 1:2])
+    tests_features = features[idx_test]
+    bf, adj_test, _ = sampler.sampling(idx_test)
 
-    loss_test = criterion(score, test_set[:, 3:4])
+    nodes_u, nodes_v = [], []
+    idx_test = idx_test.tolist()
+    for data in test_set:
+        u = int(data[0].reshape(1))
+        v = int(data[1].reshape(1))
+        for i in range(len(idx_test)):
+            if idx_test[i] == u:
+                nodes_u.append(i)
+            if idx_test[i] == v + n_users:
+                nodes_v.append(i)
+    score = model(tests_features, adj_test[0], nodes_u, nodes_v)
+    loss_test = criterion(score, test_set[:, 3:4].type(torch.FloatTensor))
     rmse_test = torch.sqrt(loss_test)
     print("Test set results:",
           "loss= {:.4f}".format(loss_test),
           'rmse_test= {:.4f}'.format(rmse_test))
-
-
-def RMSELoss(yhat, y):
-    return torch.sqrt(torch.mean((yhat - y) ** 2))
 
 
 # Train model
@@ -109,10 +134,10 @@ if __name__ == '__main__':
     print("Loading data...")
 
     adj, features, train_set, val_set, test_set, idx_train, idx_val, idx_test, n_u, n_n = utils.load_data(args.dataset)
-    features = torch.FloatTensor(features)
-    train_features = torch.FloatTensor(features[idx_train])
-    val_features = torch.FloatTensor(features[idx_val])
-    tests_features = torch.FloatTensor(features[idx_test])
+    features = torch.FloatTensor(features).to(device)
+    train_features = torch.FloatTensor(features[idx_train]).to(device)
+    val_features = torch.FloatTensor(features[idx_val]).to(device)
+    tests_features = torch.FloatTensor(features[idx_test]).to(device)
 
     print("features shape: {}".format(features.shape),
           "train_features shape: {}".format(train_features.shape),
@@ -130,17 +155,22 @@ if __name__ == '__main__':
           "num_nodes:{}".format(n_n))
 
     train_set = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
-    val_set = torch.Tensor(val_set)
-    test_set = torch.Tensor(test_set)
-    print(val_set.shape)
+    val_set = torch.Tensor(val_set).to(device)
+    test_set = torch.Tensor(test_set).to(device)
+
+    layer_sizes = [args.batch_size, args.batch_size]
+    sampler = Sampler_GCN(None, features, adj, input_dim=1, 
+                          layer_sizes=layer_sizes, device=device)
 
     # Model and optimizer
     model = GTN(in_dim=1,
                 hidden_dim=args.hidden,
                 out_dim=1,
-                dropout=args.dropout)
+                dropout=args.dropout,
+                sampler=sampler)
     print(model.__repr__())
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    # optimizer = torch.optim.RMSprop(model.parameters(), lr=args.lr, alpha=0.9)
     criterion = nn.MSELoss()
 
     if device == 'cuda':
@@ -153,11 +183,17 @@ if __name__ == '__main__':
     t_total = time.time()
     best_rmse = 9999.0
     for epoch in range(args.epochs):
-        train(epoch, args.fastmode, features, train_set, n_u, n_n)
+        loss_train, loss_val, rmse_train, rmse_val = train(epoch, args.fastmode, features, train_set, 
+                                                            val_set, idx_val, n_u, n_n, device, model)
+        print("Epoch: {}".format(epoch),
+              "loss_train: {0.4f}".format(loss_train),
+              "loss_val: {0.4f}".format(loss_val),
+              "rmse_train: {0.4f}".format(rmse_train),
+              "rmse_val:{0.4f}".format(rmse_val))
 
     print("Optimization Finished!")
     print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
     # Testing
-    test()
+    test(features, test_set, idx_test, n_u)
 
